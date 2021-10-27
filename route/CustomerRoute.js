810a3a65fd6d9ee8058afbe7d2da5c72602aa146
Rukshan123 <rukzmadu@gmail.com>
2021-10-27 17:20:16 +0530

    add all http req methods

--- route/CustomerRoute.js
+++ route/CustomerRoute.js
@@ -5,5 +5,9 @@ const CustomerController = require('../controller/CustomerController')
 const router = express.Router()
 
 router.post('/saveCustomer', CustomerController.saveCustomer)
+router.get('/getAllCustomer', CustomerController.getAllCustomers)
+router.put('/updateCustomer', CustomerController.updateCustomer)
+router.delete('/deleteCustomer', CustomerController.deleteCustomer)
+router.get('/getCustomerByID', CustomerController.getCustomerById)
 
 module.exports = router
